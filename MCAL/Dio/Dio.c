468ca05339d8c9abd1b65c881678f6cab6467142
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-07 23:32:21 +0200

    Update Dio.c

--- MCAL/Dio/Dio.c
+++ MCAL/Dio/Dio.c
@@ -1,54 +1,142 @@
-/*============================================================================
- * DIO Driver Implementation
- * author : Aysha Shaban Galal
- * Date: Nov 2025
- * Direct GPIO control for all peripherals
- *===========================================================================*/
+/*
+ * Dio.c - Digital Input/Output Driver Implementation
+ */
 
 #include "Dio.h"
-#include <avr32/io.h>
+#include <avr/io.h>
 
-Std_ReturnType Dio_Init(void) {
-    // Configure motor pins as OUTPUT
-    avr32_gpio.port[0].gpers = 0x0F;  // PA0-PA3 = GPIO output
+void Dio_Init(void) {
+    // All ports initialized as input by default
+    DDRA = 0x00;
+    DDRB = 0x00;
+    DDRC = 0x00;
+    DDRD = 0x00;
     
-    // Configure IR sensors as INPUT
-    avr32_gpio.port[1].gperc = 0x0F;  // PB0-PB3 = peripheral input
-    
-    // Configure encoder as INPUT
-    avr32_gpio.port[4].gperc = 0x03;  // PE0-PE1 = peripheral input
-    
-    // Configure I2C pins
-    avr32_gpio.port[2].gperc = 0x03;  // PC0-PC1 = I2C
-    
-    // Configure LED as OUTPUT
-    avr32_gpio.port[3].gpers = (1<<4); // PD4 = GPIO output
-    
-    return E_OK;
+    // Clear all outputs
+    PORTA = 0x00;
+    PORTB = 0x00;
+    PORTC = 0x00;
+    PORTD = 0x00;
 }
 
-Std_ReturnType Dio_WriteChannel(uint8_t Channel, Dio_LevelType Level) {
-    uint8_t port = Channel / 8;
-    uint8_t pin = Channel % 8;
+void Dio_SetPinDirection(uint8_t port, uint8_t pin, uint8_t direction) {
+    if(pin > 7) return;
     
-    if(Level == STD_HIGH) {
-        avr32_gpio.port[port].ovrs = (1 << pin);
-    } else {
-        avr32_gpio.port[port].ovrc = (1 << pin);
+    switch(port) {
+        case PORTA_REG:
+            if(direction == DIO_OUTPUT) DDRA |= (1 << pin);
+            else DDRA &= ~(1 << pin);
+            break;
+        case PORTB_REG:
+            if(direction == DIO_OUTPUT) DDRB |= (1 << pin);
+            else DDRB &= ~(1 << pin);
+            break;
+        case PORTC_REG:
+            if(direction == DIO_OUTPUT) DDRC |= (1 << pin);
+            else DDRC &= ~(1 << pin);
+            break;
+        case PORTD_REG:
+            if(direction == DIO_OUTPUT) DDRD |= (1 << pin);
+            else DDRD &= ~(1 << pin);
+            break;
     }
-    return E_OK;
 }
 
-Std_ReturnType Dio_ReadChannel(uint8_t Channel, Dio_LevelType* Level) {
-    uint8_t port = Channel / 8;
-    uint8_t pin = Channel % 8;
-    *Level = (avr32_gpio.port[port].pvr >> pin) & 1;
-    return E_OK;
+void Dio_WritePin(uint8_t port, uint8_t pin, uint8_t value) {
+    if(pin > 7) return;
+    
+    switch(port) {
+        case PORTA_REG:
+            if(value == DIO_HIGH) PORTA |= (1 << pin);
+            else PORTA &= ~(1 << pin);
+            break;
+        case PORTB_REG:
+            if(value == DIO_HIGH) PORTB |= (1 << pin);
+            else PORTB &= ~(1 << pin);
+            break;
+        case PORTC_REG:
+            if(value == DIO_HIGH) PORTC |= (1 << pin);
+            else PORTC &= ~(1 << pin);
+            break;
+        case PORTD_REG:
+            if(value == DIO_HIGH) PORTD |= (1 << pin);
+            else PORTD &= ~(1 << pin);
+            break;
+    }
 }
 
-Std_ReturnType Dio_ToggleChannel(uint8_t Channel) {
-    uint8_t port = Channel / 8;
-    uint8_t pin = Channel % 8;
-    avr32_gpio.port[port].ovrt = (1 << pin);
-    return E_OK;
+uint8_t Dio_ReadPin(uint8_t port, uint8_t pin) {
+    if(pin > 7) return 0;
+    
+    uint8_t value = 0;
+    switch(port) {
+        case PORTA_REG:
+            value = (PINA >> pin) & 0x01;
+            break;
+        case PORTB_REG:
+            value = (PINB >> pin) & 0x01;
+            break;
+        case PORTC_REG:
+            value = (PINC >> pin) & 0x01;
+            break;
+        case PORTD_REG:
+            value = (PIND >> pin) & 0x01;
+            break;
+    }
+    return value;
+}
+
+void Dio_TogglePin(uint8_t port, uint8_t pin) {
+    if(pin > 7) return;
+    
+    switch(port) {
+        case PORTA_REG:
+            PORTA ^= (1 << pin);
+            break;
+        case PORTB_REG:
+            PORTB ^= (1 << pin);
+            break;
+        case PORTC_REG:
+            PORTC ^= (1 << pin);
+            break;
+        case PORTD_REG:
+            PORTD ^= (1 << pin);
+            break;
+    }
+}
+
+void Dio_WritePort(uint8_t port, uint8_t value) {
+    switch(port) {
+        case PORTA_REG:
+            PORTA = value;
+            break;
+        case PORTB_REG:
+            PORTB = value;
+            break;
+        case PORTC_REG:
+            PORTC = value;
+            break;
+        case PORTD_REG:
+            PORTD = value;
+            break;
+    }
+}
+
+uint8_t Dio_ReadPort(uint8_t port) {
+    uint8_t value = 0;
+    switch(port) {
+        case PORTA_REG:
+            value = PINA;
+            break;
+        case PORTB_REG:
+            value = PINB;
+            break;
+        case PORTC_REG:
+            value = PINC;
+            break;
+        case PORTD_REG:
+            value = PIND;
+            break;
+    }
+    return value;
 }
