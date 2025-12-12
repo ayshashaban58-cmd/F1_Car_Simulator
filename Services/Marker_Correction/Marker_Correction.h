fb3cedc12f5355d84b3697973a5927882fff5c56
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-12 16:52:39 +0200

    Update Marker_Correction.h

--- Services/Marker_Correction/Marker_Correction.h
+++ Services/Marker_Correction/Marker_Correction.h
@@ -1,19 +1,9 @@
-/*============================================================================
- * Marker Correction Service
- * author : Aysha Shaban Galal
- * Date: Nov 2025
- * Reset odometry errors on marker detection
- *===========================================================================*/
+// src/Services/Marker_Correction/Marker_Correction.h
 
-#ifndef MARKER_CORRECTION_H_
-#define MARKER_CORRECTION_H_
+#ifndef MARKER_CORRECTION_H
+#define MARKER_CORRECTION_H
 
-#include "../Config/Std_Types.h"
-#include "../../Config/Markers_Cfg.h"
-#include "../../HAL/IR_Markers_4x/IR_Markers.h"
-#include "../Odometry/Odometry.h"
-
-Std_ReturnType Marker_Correction_Init(void);
-Std_ReturnType Marker_CorrectPosition(Detected_Marker_t MarkerID);
+void Marker_Correction_Init(void);
+void Marker_Correction_Correct(void);
 
 #endif
