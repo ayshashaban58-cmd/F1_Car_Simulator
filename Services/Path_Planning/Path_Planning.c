d645855b66f877e913f3d1fd93963c4e3cc602b2
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-12 23:55:04 +0200

    Update Path_Planning.c

--- Services/Path_Planning/Path_Planning.c
+++ Services/Path_Planning/Path_Planning.c
@@ -1,14 +1,13 @@
-// src/Services/Path_Planning/Path_Planning.c
-
 #include "Path_Planning.h"
 #include "../Odometry/Odometry.h"
+#include <math.h>
 
 const float SQUARE_SIZE = 49.5;
 uint8_t current_waypoint = 0;
-float waypoints[9][2] = { // Zigzag in 4x4 grid, example path: row1 right, row2 left, etc.
+float waypoints[9][2] = {
     {0, 0}, {4*SQUARE_SIZE, 0}, {4*SQUARE_SIZE, SQUARE_SIZE}, {0, SQUARE_SIZE},
     {0, 2*SQUARE_SIZE}, {4*SQUARE_SIZE, 2*SQUARE_SIZE}, {4*SQUARE_SIZE, 3*SQUARE_SIZE}, {0, 3*SQUARE_SIZE},
-    {0, 4*SQUARE_SIZE} // End
+    {0, 4*SQUARE_SIZE}
 };
 
 void Path_Planning_Init(void) {}
@@ -17,9 +16,9 @@ void Path_Planning_Update(void) {
     float x = Odometry_GetX();
     float y = Odometry_GetY();
     float dist = sqrt(pow(waypoints[current_waypoint][0] - x, 2) + pow(waypoints[current_waypoint][1] - y, 2));
-    if (dist < 5.0) { // Threshold
+    if (dist < 5.0) {
         current_waypoint++;
-        if (current_waypoint >= 9) current_waypoint = 0; // Loop or stop
+        if (current_waypoint >= 9) current_waypoint = 0;
     }
 }
 
