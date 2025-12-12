c0f58f38162152cbf275641768bb245bd8341952
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-12 16:39:10 +0200

    Refactor Dio.h for improved structure
    Updated header guards and function prototypes for Dio.

--- MCAL/Dio/Dio.h
+++ MCAL/Dio/Dio.h
@@ -1,34 +1,11 @@
-/*
- * Dio.h - Digital Input/Output Driver
- * ATmega32 Pin Control
- */
+// src/MCAL/Dio/Dio.h
 
-#ifndef DIO_H_
-#define DIO_H_
+#ifndef DIO_H
+#define DIO_H
 
-#include <stdint.h>
-
-// Port definitions
-#define PORTA_REG 0
-#define PORTB_REG 1
-#define PORTC_REG 2
-#define PORTD_REG 3
-
-// Pin direction
-#define DIO_INPUT  0
-#define DIO_OUTPUT 1
-
-// Pin state
-#define DIO_LOW  0
-#define DIO_HIGH 1
-
-// Function prototypes
 void Dio_Init(void);
-void Dio_SetPinDirection(uint8_t port, uint8_t pin, uint8_t direction);
-void Dio_WritePin(uint8_t port, uint8_t pin, uint8_t value);
-uint8_t Dio_ReadPin(uint8_t port, uint8_t pin);
-void Dio_TogglePin(uint8_t port, uint8_t pin);
-void Dio_WritePort(uint8_t port, uint8_t value);
-uint8_t Dio_ReadPort(uint8_t port);
+void Dio_WritePin(uint8_t pin, uint8_t value);
+uint8_t Dio_ReadPin(uint8_t pin);
+void Dio_TogglePin(uint8_t pin);
 
-#endif /* DIO_H_ */
+#endif
