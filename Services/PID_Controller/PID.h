62f1d8828897d75a255be9e3b1978f3619fb22e3
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-12 16:51:00 +0200

    Update PID.h

--- Services/PID_Controller/PID.h
+++ Services/PID_Controller/PID.h
@@ -1,32 +1,13 @@
-/*
- * PID.h - PID Controller for Steering
- * Maintains straight line using IMU gyro feedback
- */
+// src/Services/PID_Controller/PID.h
 
-#ifndef PID_H_
-#define PID_H_
+#ifndef PID_H
+#define PID_H
 
-#include <stdint.h>
-
-// PID parameters structure
-typedef struct {
-    float kp;
-    float ki;
-    float kd;
-    float error;
-    float last_error;
-    float integral;
-    float derivative;
-    float output;
-    float setpoint;
-} PID_Controller_t;
-
-// Function prototypes
 void PID_Init(void);
 void PID_Update(void);
 void PID_Reset(void);
-void PID_SetGains(float kp, float ki, float kd);
-void PID_SetSetpoint(float setpoint);
-float PID_GetOutput(void);
 
-#endif /* PID_H_ */
+extern float pid_speed_output;
+extern float pid_steering_output;
+
+#endif
