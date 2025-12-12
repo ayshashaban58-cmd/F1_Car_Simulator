c9185feec2767991bb415d922d236d747d609677
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-12 23:52:24 +0200

    Update IR_Markers.h

--- HAL/IR_Markers_4x/IR_Markers.h
+++ HAL/IR_Markers_4x/IR_Markers.h
@@ -1,10 +1,8 @@
-// src/HAL/IR_Markers_4x/IR_Markers.h
-
 #ifndef IR_MARKERS_H
 #define IR_MARKERS_H
 
 void IR_Markers_Init(void);
 void IR_Markers_Scan(void);
-uint8_t IR_Markers_GetPattern(void); // Returns detected marker ID or 0
+uint8_t IR_Markers_GetPattern(void); // Returns marker ID 1-4 or 0
 
 #endif
