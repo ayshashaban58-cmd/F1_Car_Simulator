c65520176c5b22a6173f4298267f32814afbd6c8
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-12 23:45:22 +0200

    Update Motor.h

--- HAL/Motor_L298N/Motor.h
+++ HAL/Motor_L298N/Motor.h
@@ -1,11 +1,7 @@
-// src/HAL/Motor_L298N/Motor.h
-
 #ifndef MOTOR_H
 #define MOTOR_H
 
 void Motor_Init(void);
 void Motor_SetSpeed(int16_t speed); // -255 to 255
-void Motor_SetDirection(uint8_t dir); // 0 forward, 1 backward
-void Motor_Stop(void);
 
 #endif
