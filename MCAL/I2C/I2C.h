9ee7ff73043dbc5df7c6a4c0b566b9a46bcdadb3
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-12 16:41:47 +0200

    Update I2C.h

--- MCAL/I2C/I2C.h
+++ MCAL/I2C/I2C.h
@@ -1,25 +1,10 @@
-/*
- * I2C.h - I2C/TWI Driver for MPU6050
- * SCL: PC0, SDA: PC1
- */
+// src/MCAL/I2C/I2C.h
 
-#ifndef I2C_H_
-#define I2C_H_
+#ifndef I2C_H
+#define I2C_H
 
-#include <stdint.h>
-
-// I2C status codes
-#define I2C_SUCCESS 0
-#define I2C_ERROR   1
-
-// Function prototypes
 void I2C_Init(void);
-uint8_t I2C_Start(void);
-void I2C_Stop(void);
-uint8_t I2C_WriteByte(uint8_t data);
-uint8_t I2C_ReadByte(uint8_t *data, uint8_t ack);
-uint8_t I2C_WriteRegister(uint8_t device_addr, uint8_t reg_addr, uint8_t data);
-uint8_t I2C_ReadRegister(uint8_t device_addr, uint8_t reg_addr, uint8_t *data);
-uint8_t I2C_ReadMultipleRegisters(uint8_t device_addr, uint8_t reg_addr, uint8_t *buffer, uint8_t length);
+void I2C_Write(uint8_t addr, uint8_t reg, uint8_t data);
+uint8_t I2C_Read(uint8_t addr, uint8_t reg);
 
-#endif /* I2C_H_ */
+#endif
