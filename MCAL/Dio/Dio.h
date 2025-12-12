8b2aa402790118d3e025b7cea863ae48ce625ed5
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-12 23:42:27 +0200

    Update Dio.h

--- MCAL/Dio/Dio.h
+++ MCAL/Dio/Dio.h
@@ -1,11 +1,9 @@
-// src/MCAL/Dio/Dio.h
-
 #ifndef DIO_H
 #define DIO_H
 
 void Dio_Init(void);
-void Dio_WritePin(uint8_t pin, uint8_t value);
-uint8_t Dio_ReadPin(uint8_t pin);
-void Dio_TogglePin(uint8_t pin);
+void Dio_WritePin(char port, uint8_t bit, uint8_t value); // e.g., 'B', PB0, 1
+uint8_t Dio_ReadPin(char port, uint8_t bit);
+void Dio_TogglePin(char port, uint8_t bit);
 
 #endif
