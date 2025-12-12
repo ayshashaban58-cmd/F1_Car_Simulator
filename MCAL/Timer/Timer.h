0f124f2e4ea6711ca2058f79eb52af72ecdab1ef
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-12 16:40:40 +0200

    Refactor Timer.h header guard and function names

--- MCAL/Timer/Timer.h
+++ MCAL/Timer/Timer.h
@@ -1,21 +1,10 @@
-/*
- * Timer.h - Timer Driver for PWM and System Tick
- * Timer0: 100Hz system tick (CTC mode)
- * Timer1: PWM for motor and servo
- */
+// src/MCAL/Timer/Timer.h
 
-#ifndef TIMER_H_
-#define TIMER_H_
+#ifndef TIMER_H
+#define TIMER_H
 
-#include <stdint.h>
-
-// PWM channels
-#define PWM_MOTOR   0  // OC1A (PD5)
-#define PWM_SERVO   1  // OC1B (PD4)
-
-// Function prototypes
 void Timer_Init(void);
 void Timer_SetPWM(uint8_t channel, uint16_t duty);
-uint32_t Timer_GetTicks(void);
+uint32_t Timer_GetTick(void);
 
-#endif /* TIMER_H_ */
+#endif
