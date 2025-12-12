bfeb4170608bb2505ce5622b3695ac3ba4a76018
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-12 23:53:22 +0200

    Update Odometry.h

--- Services/Odometry/Odometry.h
+++ Services/Odometry/Odometry.h
@@ -5,8 +5,6 @@
  * IMU + Single Encoder fusion
  *===========================================================================*/
 
-// src/Services/Odometry/Odometry.h
-
 #ifndef ODOMETRY_H
 #define ODOMETRY_H
 
@@ -15,11 +13,5 @@ void Odometry_Update(void);
 float Odometry_GetX(void);
 float Odometry_GetY(void);
 float Odometry_GetTheta(void);
-
-#endif
-extern Position_t CarPosition;
-
-Std_ReturnType Odometry_Init(float32_t StartX, float32_t StartY);
-Std_ReturnType Odometry_Update(void);
 
 #endif
