c239c11f8496e1a2e133c520cfb9d81ae4f35048
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-12 23:43:04 +0200

    Update Timer.h

--- MCAL/Timer/Timer.h
+++ MCAL/Timer/Timer.h
@@ -1,10 +1,8 @@
-// src/MCAL/Timer/Timer.h
-
 #ifndef TIMER_H
 #define TIMER_H
 
 void Timer_Init(void);
-void Timer_SetPWM(uint8_t channel, uint16_t duty);
+void Timer_SetPWM(uint8_t channel, uint16_t duty); // channel 0 for Motor (Timer0), 1 for Servo (Timer1)
 uint32_t Timer_GetTick(void);
 
 #endif
