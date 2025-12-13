8be211c98a1b141bc1d27ef0c0a15b2d83c21848
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-13 09:43:36 +0200

    Update Motor.c

--- HAL/Motor_L298N/Motor.c
+++ HAL/Motor_L298N/Motor.c
@@ -1,25 +1,41 @@
 #include "Motor.h"
-#include "../../MCAL/Dio/Dio.h"
-#include "../../MCAL/Timer/Timer.h"
 
 void Motor_Init(void) {
-    Dio_WritePin('B', PB0, 0);
-    Dio_WritePin('B', PB1, 0);
+    Dio_SetPinDirection(MOTOR_IN1_PORT, MOTOR_IN1_PIN, DIO_OUTPUT);
+    Dio_SetPinDirection(MOTOR_IN2_PORT, MOTOR_IN2_PIN, DIO_OUTPUT);
+    Dio_SetPinDirection(MOTOR_PWM_PORT, MOTOR_PWM_PIN, DIO_OUTPUT);
+    Motor_Stop();
 }
 
-void Motor_SetSpeed(int16_t speed) {
-    uint16_t abs_speed = (speed < 0) ? -speed : speed;
-    Timer_SetPWM(0, abs_speed); // Channel 0 for Motor
+void Motor_SetSpeed(uint8_t speed) {
+    if(speed > 100) speed = 100;
+    Timer_SetPWM(TIMER1_CHANNELA, speed);
+}
 
-    if (speed > 0) {
-        Dio_WritePin('B', PB0, 1);
-        Dio_WritePin('B', PB1, 0);
-    } else if (speed < 0) {
-        Dio_WritePin('B', PB0, 0);
-        Dio_WritePin('B', PB1, 1);
-    } else {
-        Dio_WritePin('B', PB0, 0);
-        Dio_WritePin('B', PB1, 0);
-        Timer_SetPWM(0, 0);
+void Motor_SetDirection(Motor_DirectionType dir) {
+    switch(dir) {
+        case MOTOR_FORWARD:
+            Dio_WritePin(MOTOR_IN1_PORT, MOTOR_IN1_PIN, DIO_HIGH);
+            Dio_WritePin(MOTOR_IN2_PORT, MOTOR_IN2_PIN, DIO_LOW);
+            break;
+        case MOTOR_BACKWARD:
+            Dio_WritePin(MOTOR_IN1_PORT, MOTOR_IN1_PIN, DIO_LOW);
+            Dio_WritePin(MOTOR_IN2_PORT, MOTOR_IN2_PIN, DIO_HIGH);
+            break;
+        case MOTOR_STOP:
+        default:
+            Dio_WritePin(MOTOR_IN1_PORT, MOTOR_IN1_PIN, DIO_LOW);
+            Dio_WritePin(MOTOR_IN2_PORT, MOTOR_IN2_PIN, DIO_LOW);
+            break;
     }
+}
+
+void Motor_Stop(void) {
+    Motor_SetDirection(MOTOR_STOP);
+    Motor_SetSpeed(0);
+}
+
+void Motor_Drive(Motor_DirectionType dir, uint8_t speed) {
+    Motor_SetDirection(dir);
+    Motor_SetSpeed(speed);
 }
