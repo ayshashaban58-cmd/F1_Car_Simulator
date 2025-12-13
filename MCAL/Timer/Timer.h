9a7848a41a779b48804177ba7a610ee220909bab
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-13 09:38:06 +0200

    Update Timer.h

--- MCAL/Timer/Timer.h
+++ MCAL/Timer/Timer.h
@@ -1,8 +1,23 @@
-#ifndef TIMER_H
-#define TIMER_H
+#ifndef TIMER_H_
+#define TIMER_H_
 
-void Timer_Init(void);
-void Timer_SetPWM(uint8_t channel, uint16_t duty); // channel 0 for Motor (Timer0), 1 for Servo (Timer1)
-uint32_t Timer_GetTick(void);
+#include <avr/io.h>
+#include <avr/interrupt.h>
 
-#endif
+// Timer channels for PWM
+typedef enum {
+    TIMER0_CHANNEL = 0,
+    TIMER1_CHANNELA,
+    TIMER1_CHANNELB,
+    TIMER2_CHANNEL
+} Timer_ChannelType;
+
+// Function Prototypes
+void Timer0_Init(void);
+void Timer1_Init_PWM(void);
+void Timer2_Init(void);
+void Timer_SetPWM(Timer_ChannelType channel, uint8_t duty);
+uint32_t Timer_GetSystemTick(void);
+void Timer_DelayMs(uint16_t ms);
+
+#endif /* TIMER_H_ */
