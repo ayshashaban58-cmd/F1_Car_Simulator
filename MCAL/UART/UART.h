ed779eba866cb78ca6a974b08558fcfbbd0293eb
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-13 09:41:16 +0200

    Update UART.h

--- MCAL/UART/UART.h
+++ MCAL/UART/UART.h
@@ -1,9 +1,17 @@
-#ifndef UART_H
-#define UART_H
+#ifndef UART_H_
+#define UART_H_
 
-void UART_Init(void);
-void UART_SendByte(uint8_t byte);
-uint8_t UART_ReceiveByte(void);
+#include <avr/io.h>
+#include <avr/interrupt.h>
+
+#define UART_BUFFER_SIZE 128
+
+// Function Prototypes
+void UART_Init(uint32_t baudrate);
+void UART_SendByte(uint8_t data);
 void UART_SendString(const char* str);
+uint8_t UART_Available(void);
+uint8_t UART_ReceiveByte(void);
+void UART_Flush(void);
 
-#endif
+#endif /* UART_H_ */
