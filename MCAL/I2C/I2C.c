b979a3ebbd8bf74aa43fcca12fb9b0248568b2e5
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-13 09:39:31 +0200

    Update I2C.c

--- MCAL/I2C/I2C.c
+++ MCAL/I2C/I2C.c
@@ -1,47 +1,105 @@
-#include <avr/io.h>
 #include "I2C.h"
 
 void I2C_Init(void) {
-    TWBR = 72; // 100kHz at 8MHz
-    TWSR = 0;
+    // Set SCL to 100kHz: TWBR = ((F_CPU/SCL_FREQ)-16)/(2*Prescaler)
+    // F_CPU = 8MHz, SCL = 100kHz, Prescaler = 1
+    TWBR = 32; // (8000000/100000 - 16)/2 = 32
+    TWSR = 0x00; // Prescaler = 1
 }
 
-void I2C_Start(void) {
+I2C_StatusType I2C_Start(void) {
     TWCR = (1 << TWINT) | (1 << TWSTA) | (1 << TWEN);
-    while (!(TWCR & (1 << TWINT)));
+    while(!(TWCR & (1 << TWINT)));
+    if((TWSR & 0xF8) != I2C_START && (TWSR & 0xF8) != I2C_REP_START) {
+        return I2C_ERROR;
+    }
+    return I2C_OK;
+}
+
+I2C_StatusType I2C_Write(uint8_t data) {
+    TWDR = data;
+    TWCR = (1 << TWINT) | (1 << TWEN);
+    while(!(TWCR & (1 << TWINT)));
+    uint8_t status = TWSR & 0xF8;
+    if(status != I2C_MT_SLA_ACK && status != I2C_MT_DATA_ACK && status != I2C_MR_SLA_ACK) {
+        return I2C_ERROR;
+    }
+    return I2C_OK;
+}
+
+uint8_t I2C_ReadACK(void) {
+    TWCR = (1 << TWINT) | (1 << TWEN) | (1 << TWEA);
+    while(!(TWCR & (1 << TWINT)));
+    return TWDR;
+}
+
+uint8_t I2C_ReadNACK(void) {
+    TWCR = (1 << TWINT) | (1 << TWEN);
+    while(!(TWCR & (1 << TWINT)));
+    return TWDR;
 }
 
 void I2C_Stop(void) {
     TWCR = (1 << TWINT) | (1 << TWSTO) | (1 << TWEN);
 }
 
-void I2C_WriteByte(uint8_t byte) {
-    TWDR = byte;
-    TWCR = (1 << TWINT) | (1 << TWEN);
-    while (!(TWCR & (1 << TWINT)));
-}
-
-uint8_t I2C_ReadByte(uint8_t ack) {
-    TWCR = (1 << TWINT) | (1 << TWEN) | (ack << TWEA);
-    while (!(TWCR & (1 << TWINT)));
-    return TWDR;
-}
-
-void I2C_Write(uint8_t addr, uint8_t reg, uint8_t data) {
-    I2C_Start();
-    I2C_WriteByte(addr << 1);
-    I2C_WriteByte(reg);
-    I2C_WriteByte(data);
+I2C_StatusType I2C_WriteRegister(uint8_t devAddr, uint8_t regAddr, uint8_t data) {
+    if(I2C_Start() != I2C_OK) return I2C_ERROR;
+    if(I2C_Write((devAddr << 1) | 0) != I2C_OK) {
+        I2C_Stop();
+        return I2C_ERROR;
+    }
+    if(I2C_Write(regAddr) != I2C_OK) {
+        I2C_Stop();
+        return I2C_ERROR;
+    }
+    if(I2C_Write(data) != I2C_OK) {
+        I2C_Stop();
+        return I2C_ERROR;
+    }
     I2C_Stop();
+    return I2C_OK;
 }
 
-uint8_t I2C_Read(uint8_t addr, uint8_t reg) {
-    I2C_Start();
-    I2C_WriteByte(addr << 1);
-    I2C_WriteByte(reg);
-    I2C_Start();
-    I2C_WriteByte((addr << 1) | 1);
-    uint8_t data = I2C_ReadByte(0);
+I2C_StatusType I2C_ReadRegister(uint8_t devAddr, uint8_t regAddr, uint8_t* data) {
+    if(I2C_Start() != I2C_OK) return I2C_ERROR;
+    if(I2C_Write((devAddr << 1) | 0) != I2C_OK) {
+        I2C_Stop();
+        return I2C_ERROR;
+    }
+    if(I2C_Write(regAddr) != I2C_OK) {
+        I2C_Stop();
+        return I2C_ERROR;
+    }
+    if(I2C_Start() != I2C_OK) return I2C_ERROR;
+    if(I2C_Write((devAddr << 1) | 1) != I2C_OK) {
+        I2C_Stop();
+        return I2C_ERROR;
+    }
+    *data = I2C_ReadNACK();
     I2C_Stop();
-    return data;
+    return I2C_OK;
+}
+
+I2C_StatusType I2C_ReadMultiple(uint8_t devAddr, uint8_t regAddr, uint8_t* buffer, uint8_t length) {
+    if(I2C_Start() != I2C_OK) return I2C_ERROR;
+    if(I2C_Write((devAddr << 1) | 0) != I2C_OK) {
+        I2C_Stop();
+        return I2C_ERROR;
+    }
+    if(I2C_Write(regAddr) != I2C_OK) {
+        I2C_Stop();
+        return I2C_ERROR;
+    }
+    if(I2C_Start() != I2C_OK) return I2C_ERROR;
+    if(I2C_Write((devAddr << 1) | 1) != I2C_OK) {
+        I2C_Stop();
+        return I2C_ERROR;
+    }
+    for(uint8_t i = 0; i < length - 1; i++) {
+        buffer[i] = I2C_ReadACK();
+    }
+    buffer[length - 1] = I2C_ReadNACK();
+    I2C_Stop();
+    return I2C_OK;
 }
