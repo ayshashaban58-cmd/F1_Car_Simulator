e8c5b1be25d380256672556c4db0e305256dc9b5
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-13 13:04:00 +0200

    Update Telemetry.h

--- Application/UART_Telemetry/Telemetry.h
+++ Application/UART_Telemetry/Telemetry.h
@@ -1,7 +1,29 @@
-#ifndef TELEMETRY_H
-#define TELEMETRY_H
+#ifndef TELEMETRY_H_
+#define TELEMETRY_H_
 
-void Telemetry_Send(void);
-void Telemetry_ParseCommand(uint8_t cmd);
+#include <stdint.h>
+#include "../MCAL/UART/UART.h"
+#include "../Services/Odometry/Odometry.h"
+#include "../HAL/MPU6050/MPU6050.h"
+#include "../HAL/IR_Markers_4x/IR_Markers.h"
+#include "../Application/F1_Car/F1_Car.h"
 
-#endif
+// Command definitions
+#define CMD_START 'S'
+#define CMD_STOP 'P'
+#define CMD_RESET 'R'
+#define CMD_SPEED 'V'
+
+typedef struct {
+    char command;
+    uint8_t value;
+} Telemetry_CommandType;
+
+// Function Prototypes
+void Telemetry_Init(void);
+void Telemetry_SendData(Odometry_DataType* odom, MPU6050_DataType* imu, 
+                        IR_Markers_DataType* irData, F1_Car_Type* car);
+uint8_t Telemetry_ReceiveCommand(Telemetry_CommandType* cmd);
+void Telemetry_ProcessCommand(Telemetry_CommandType* cmd, F1_Car_Type* car);
+
+#endif /* TELEMETRY_H_ */
