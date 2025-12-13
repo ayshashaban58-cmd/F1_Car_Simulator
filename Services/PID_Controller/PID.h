ab257f87f83715d4bd23959bbc1ef1172db8c56d
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-13 12:59:34 +0200

    Update PID.h

--- Services/PID_Controller/PID.h
+++ Services/PID_Controller/PID.h
@@ -1,11 +1,22 @@
-#ifndef PID_H
-#define PID_H
+#ifndef PID_H_
+#define PID_H_
 
-void PID_Init(void);
-void PID_Update(void);
-void PID_Reset(void);
+#include <stdint.h>
 
-extern float pid_speed_output;
-extern float pid_steering_output;
+typedef struct {
+    float kp;
+    float ki;
+    float kd;
+    float integral;
+    float prevError;
+    float output;
+    float minOutput;
+    float maxOutput;
+} PID_ControllerType;
 
-#endif
+// Function Prototypes
+void PID_Init(PID_ControllerType* pid, float kp, float ki, float kd, float minOut, float maxOut);
+float PID_Update(PID_ControllerType* pid, float error, float dt);
+void PID_Reset(PID_ControllerType* pid);
+
+#endif /* PID_H_ */
