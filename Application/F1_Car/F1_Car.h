94b806a6d24bb008ac2c3dca917cde296cd8cbfd
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-13 13:03:10 +0200

    Update F1_Car.h

--- Application/F1_Car/F1_Car.h
+++ Application/F1_Car/F1_Car.h
@@ -1,10 +1,27 @@
-#ifndef F1_CAR_H
-#define F1_CAR_H
+#ifndef F1_CAR_H_
+#define F1_CAR_H_
 
-void F1_Car_Init(void);
-void F1_Car_Update(void);
-void F1_Car_Start(void);
-void F1_Car_Stop(void);
-void F1_Car_Reset(void);
+#include <stdint.h>
 
-#endif
+typedef enum {
+    CAR_IDLE = 0,
+    CAR_RUNNING,
+    CAR_STOPPED,
+    CAR_ERROR
+} F1_Car_StateType;
+
+typedef struct {
+    F1_Car_StateType state;
+    uint8_t motorSpeed;
+    uint8_t isRunning;
+} F1_Car_Type;
+
+// Function Prototypes
+void F1_Car_Init(F1_Car_Type* car);
+void F1_Car_Start(F1_Car_Type* car);
+void F1_Car_Stop(F1_Car_Type* car);
+void F1_Car_Reset(F1_Car_Type* car);
+void F1_Car_SetSpeed(F1_Car_Type* car, uint8_t speed);
+F1_Car_StateType F1_Car_GetState(F1_Car_Type* car);
+
+#endif /* F1_CAR_H_ */
