4b2d273806ad1fa30dbfb5be23c8f604edd622d7
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-13 13:02:11 +0200

    Update Marker_Correction.h

--- Services/Marker_Correction/Marker_Correction.h
+++ Services/Marker_Correction/Marker_Correction.h
@@ -1,7 +1,22 @@
-#ifndef MARKER_CORRECTION_H
-#define MARKER_CORRECTION_H
+#ifndef MARKER_CORRECTION_H_
+#define MARKER_CORRECTION_H_
 
-void Marker_Correction_Init(void);
-void Marker_Correction_Correct(void);
+#include <stdint.h>
+#include "../HAL/IR_Markers_4x/IR_Markers.h"
+#include "../Services/Odometry/Odometry.h"
+#include "../HAL/MPU6050/MPU6050.h"
 
-#endif
+typedef struct {
+    uint8_t marker1Passed;
+    uint8_t marker2Passed;
+    uint8_t marker3Passed;
+    uint8_t marker4Passed;
+    uint8_t expectedSquares[4]; // Expected squares for each marker
+} Marker_CorrectionType;
+
+// Function Prototypes
+void Marker_Init(Marker_CorrectionType* marker);
+void Marker_CheckAndCorrect(Marker_CorrectionType* marker, IR_Markers_DataType* irData, 
+                            Odometry_DataType* odom, MPU6050_DataType* imu);
+
+#endif /* MARKER_CORRECTION_H_ */
