c080032ac72d03ec060313d48c88bb3f9a926a38
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-13 09:46:27 +0200

    Update MPU6050.h

--- HAL/MPU6050/MPU6050.h
+++ HAL/MPU6050/MPU6050.h
@@ -1,10 +1,33 @@
-#ifndef MPU6050_H
-#define MPU6050_H
+#ifndef MPU6050_H_
+#define MPU6050_H_
 
+#include <stdint.h>
+#include "../MCAL/I2C/I2C.h"
+
+// MPU6050 I2C Address
 #define MPU6050_ADDR 0x68
 
-void MPU6050_Init(void);
-void MPU6050_ReadAccel(int16_t* ax, int16_t* ay, int16_t* az);
-void MPU6050_ReadGyro(int16_t* gx, int16_t* gy, int16_t* gz);
+// MPU6050 Registers
+#define MPU6050_PWR_MGMT_1 0x6B
+#define MPU6050_GYRO_CONFIG 0x1B
+#define MPU6050_ACCEL_CONFIG 0x1C
+#define MPU6050_ACCEL_XOUT_H 0x3B
+#define MPU6050_GYRO_XOUT_H 0x43
 
-#endif
+typedef struct {
+    int16_t accelX;
+    int16_t accelY;
+    int16_t accelZ;
+    int16_t gyroX;
+    int16_t gyroY;
+    int16_t gyroZ;
+    float angleZ; // Yaw angle in degrees
+} MPU6050_DataType;
+
+// Function Prototypes
+void MPU6050_Init(void);
+void MPU6050_ReadRawData(MPU6050_DataType* data);
+void MPU6050_UpdateAngle(MPU6050_DataType* data, float dt);
+void MPU6050_ResetAngle(MPU6050_DataType* data);
+
+#endif /* MPU6050_H_ */
