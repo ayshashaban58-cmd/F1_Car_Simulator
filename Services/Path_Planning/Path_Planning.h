f95b64eddd459b30e5609b0cd1dbccf5703d4ca2
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-13 13:01:14 +0200

    Update Path_Planning.h

--- Services/Path_Planning/Path_Planning.h
+++ Services/Path_Planning/Path_Planning.h
@@ -1,10 +1,26 @@
-#ifndef PATH_PLANNING_H
-#define PATH_PLANNING_H
+#ifndef PATH_PLANNING_H_
+#define PATH_PLANNING_H_
 
-void Path_Planning_Init(void);
-void Path_Planning_Update(void);
-float Path_Planning_GetTargetX(void);
-float Path_Planning_GetTargetY(void);
-uint8_t Path_Planning_CheckLap(void);
+#include <stdint.h>
+#include "../Services/Odometry/Odometry.h"
 
-#endif
+typedef enum {
+    PATH_STRAIGHT = 0,
+    PATH_TURN_LEFT,
+    PATH_COMPLETED
+} Path_StateType;
+
+typedef struct {
+    Path_StateType state;
+    uint8_t squareTarget; // Number of squares to travel before turning
+    float targetAngle; // Target angle for turning
+    uint8_t turnCount; // Number of turns completed
+} Path_PlanningType;
+
+// Function Prototypes
+void Path_Init(Path_PlanningType* path);
+void Path_Update(Path_PlanningType* path, Odometry_DataType* odom);
+float Path_GetAngleError(Path_PlanningType* path, Odometry_DataType* odom);
+uint8_t Path_IsCompleted(Path_PlanningType* path);
+
+#endif /* PATH_PLANNING_H_ */
