5be0fb07fa9f4f414c74119946be9038eb7025c5
ayshashaban58-cmd <ayshashaban58@gmail.com>
2025-12-13 09:44:42 +0200

    Update Servo.c

--- HAL/Servo/Servo.c
+++ HAL/Servo/Servo.c
@@ -1,11 +1,25 @@
 #include "Servo.h"
-#include "../../MCAL/Timer/Timer.h"
 
 void Servo_Init(void) {
-    // Already in Timer_Init
+    Dio_SetPinDirection(SERVO_PWM_PORT, SERVO_PWM_PIN, DIO_OUTPUT);
+    Servo_Center();
 }
 
-void Servo_SetAngle(int8_t angle) {
-    uint16_t pulse = 1500 + (angle * 500 / 90); // 1000-2000us
-    Timer_SetPWM(1, pulse); // Channel 1 for Servo
+void Servo_SetAngle(uint8_t angle) {
+    if(angle > 100) angle = 100;
+    Timer_SetPWM(TIMER1_CHANNELB, angle);
+}
+
+void Servo_Center(void) {
+    Servo_SetAngle(SERVO_CENTER);
+}
+
+void Servo_TurnLeft(uint8_t amount) {
+    if(amount > 50) amount = 50;
+    Servo_SetAngle(SERVO_CENTER - amount);
+}
+
+void Servo_TurnRight(uint8_t amount) {
+    if(amount > 50) amount = 50;
+    Servo_SetAngle(SERVO_CENTER + amount);
 }
